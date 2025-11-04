bdbe80e0aab67a9f77e72fc4d19a2ee51dddb917
yldrmahmet <ay689089@gmail.com>
2025-11-04 13:07:57 +0300

    Fix shuffleArray exercise

--- extras/06_shuffleArray.js
+++ extras/06_shuffleArray.js
@@ -1,22 +1,20 @@
 let arr = [1, 2, 3, 4, 5];
 
 const shuffle = (arr) => {
-  const randomArr = [];
-  while (randomArr.length !== 5) {
-    let randomIndex = Math.floor(Math.random() * arr.length);
-    let randomNumber = arr[randomIndex];
-    if (!randomArr.includes(randomNumber)) {
-      randomArr.push(randomNumber);
-    }
+  for (let i = arr.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
   }
-  console.log(randomArr);
 };
 
 shuffle(arr);
-// arr = [3, 2, 1]
+console.log(arr);
 
+// arr = [3, 2, 1]
 shuffle(arr);
+console.log(arr);
 // arr = [2, 1, 3]
 
 shuffle(arr);
+console.log(arr);
 // arr = [3, 1, 2]
